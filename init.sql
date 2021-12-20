249385e201ce3471f8677b08db615f022c15f7c5
Dipanjan Haldar <dipanjan.haldar@gmail.com>
2021-12-20 17:13:05 -0500

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE AIRPORT (id INTEGER NOT NULL PRIMARY KEY,
-    apt_code VARCHAR(255),
-    apt_name VARCHAR(255),
-    city_name VARCHAR(255),
-    country VARCHAR(255)
-);
-
-create sequence SEQ_GEN_IDENTITY;
